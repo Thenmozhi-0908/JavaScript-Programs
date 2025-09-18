4f536db3aac037b3241674fdf3cccb4233ad1746
Thenmozhi-0908 <thenmozhipandi2006@gmail.com>
2025-09-18 18:15:51 +0530

    dayone_ex

---
+++ ex_1.js
@@ -0,0 +1,19 @@
+let  firstName = "Honey"
+console.log("Print the data " + firstName);
+console.log("Print the type of the data " + typeof(firstName));
+
+let  companyName = null;
+console.log("Print the data " + companyName);
+console.log("Print the type of the data " + typeof(companyName));
+
+let  mobileNumber = 8886565434;
+console.log("Print the data " + mobileNumber);
+console.log("Print the type of the data " + typeof(mobileNumber));
+
+let   isAutomationpowerful = true;
+console.log("Print the data " + isAutomationpowerful);
+console.log("Print the type of the data " + typeof(isAutomationpowerful));
+
+let   hasPlaywright ;
+console.log("Print the data " + hasPlaywright);
+console.log("Print the type of the data " + typeof(hasPlaywright));
