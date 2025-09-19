de7a21c32d63f5851d0519e7a6d0452e0067256f
Thenmozhi-0908 <thenmozhipandi2006@gmail.com>
2025-09-19 11:29:05 +0530

    Add files via upload

---
+++ numbertype_ex.js
@@ -0,0 +1,13 @@
+function numberType(x) {
+    if (x>0) {
+        console.log("Input is positive number");
+        
+    }
+    else if (x<0) {
+         console.log("Input is Negative number");
+    }
+    else{
+     console.log("Input is nutral number");
+}
+}
+numberType(9);
