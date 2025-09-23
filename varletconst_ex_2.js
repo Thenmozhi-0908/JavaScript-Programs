6a303d04b2c43e8115511f8e063d2411ed00eb21
Thenmozhi-0908 <thenmozhipandi2006@gmail.com>
2025-09-23 19:00:14 +0530

    Add files via upload

---
+++ varletconst_ex_2.js
@@ -0,0 +1,16 @@
+const browserName = "chrome"
+function getbrowsername() {
+    if (browserName === "chrome") {
+         //var browserName = "chrome"    // browser input is  undefined  why its giving undefined?
+        let browserName = "chrome"      //browser input is chrome
+}
+console.log(`browser input is  ${browserName}`) 
+}
+getbrowsername();
+
+
+
+
+
+
+
