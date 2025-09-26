1306a98028ae7a9d3086696abf6d00076f9d2b4c
Thenmozhi-0908 <thenmozhipandi2006@gmail.com>
2025-09-26 13:32:37 +0530

    Update reversestring_ex.js

--- reversestring_ex.js
+++ reversestring_ex.js
@@ -5,12 +5,14 @@ console.log(ch);
 
 let reversed = "";
 for (let i =((companyNmae.length)-1); i >= 0; i--) {
-   reversed += ch[i];
+   reversed= reversed + ch[i];
 }
    console.log(reversed);
 }
 stringreversal();
 
+// shorcut using inbuild method reverse
 
-
-
+let bank = "Testleaf";
+let newName = bank.split("").reverse().join("");
+console.log(newName);
