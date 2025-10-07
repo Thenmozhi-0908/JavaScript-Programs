96b7b7718b9e8cdbc99e2b600fb64f266d52e3b5
Thenmozhi-0908 <thenmozhipandi2006@gmail.com>
2025-10-07 16:08:46 +0530

    Add files via upload

---
+++ sflogin_ex.spec.ts
@@ -0,0 +1,24 @@
+ import {chromium, test } from "@playwright/test"
+test("Test of Launch browser and login to Salaesforce", async ({page}) =>{
+
+    // Browser launch
+await page.goto("https://login.salesforce.com/?locale=in")
+
+//login screen
+await page.locator(`#username`).fill(`thenmozhipandi2006-dgjq@force.com`);
+await page.locator(`#password`).fill(`Allthebest@1`);
+await page.locator(`#Login`).click();
+
+// print details
+setTimeout(() => {
+    const Title =  page.title();
+console.log("Current page Title is  " +Title );
+    
+const currntUrl =  page.url();
+console.log("Current page url is  " + currntUrl );
+}, 10000);
+
+// close the browser
+await page.close();
+
+})
